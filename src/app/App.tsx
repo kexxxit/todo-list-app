ab9f0b72f344f7271021975c357f781cf010611e
kexxxit <kvashevich80@gmail.com>
2024-08-06 18:07:17 +0500

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { Main } from '../pages/main'
+
+function App() {
+    return (
+        <div className='container mx-auto px-16 text-lg h-full'>
+            <Main />
+        </div>
+    )
+}
+
+export default App
---
+++ src/pages/main/ui/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Main = () => {
+    return <div className='grid grid-cols-2 h-full'>
+        <div className='h-full'>список</div>
+        <div className='bg-gray-300 h-full'>детализация</div>
+    </div>
+}
